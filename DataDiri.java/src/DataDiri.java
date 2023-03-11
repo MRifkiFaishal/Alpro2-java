5e8e1de0fd74b308c9ae60e9ebf4477219b9afd6
MRifkiFaishal <muhammadrifkifaishal@gmail.com>
2023-03-11 12:01:06 +0700

    comit pertama

---
+++ DataDiri.java/src/DataDiri.java
@@ -0,0 +1,18 @@
+public class DataDiri {
+    public static void main(String[] args) throws Exception {
+    String nama = "Muhammad Rifki Faishal";
+    String npm = "2022320013";
+    String jurusan = "Sistem Informasi" ;
+    String alamat = "Alamat: Jl. Cut Mutia ";
+    String kota = "Kota: Bekasi";
+    String provinsi = "Provinsi: Jawa Barat";
+    
+
+    System.out.println(nama);
+    System.out.println(npm);
+    System.out.println(jurusan);
+    System.out.println(alamat);
+    System.out.println(kota);
+    System.out.println(provinsi);
+    }
+}
